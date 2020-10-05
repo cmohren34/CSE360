74aef81a68ffff02551eb6b64753e12389c1b12e
Carlos Moreno <cmoren34@asu.edu>
2020-10-05 15:32:12 -0700

    testing github connection

--- Assignment02/AddingMachine.java
+++ Assignment02/AddingMachine.java
@@ -3,10 +3,6 @@ package cse360assignment02;
 public class AddingMachine {
   private int total;
   
-  public AddingMachine () {
-    total = 0;  // not needed - included for clarity
-  }
-  
   public int getTotal () {
     return 0;
   }
