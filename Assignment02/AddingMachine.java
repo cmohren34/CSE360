57ef3febf352dc3d5f5244dd3c7aa6e219e8d321
Carlos Moreno <cmoren34@asu.edu>
2020-10-05 15:34:09 -0700

    This is the initial version of the AddingMachine class

--- Assignment02/AddingMachine.java
+++ Assignment02/AddingMachine.java
@@ -3,6 +3,10 @@ package cse360assignment02;
 public class AddingMachine {
   private int total;
   
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
   public int getTotal () {
     return 0;
   }
