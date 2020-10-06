909e2c37320ae9ff478eee525aba44f56d361d23
Carlos Moreno <cmoren34@asu.edu>
2020-10-05 18:31:21 -0700

    changed the names of files to meet the submission requirements

--- Assignment02/AddingMachine.java
+++ Assignment02/AddingMachine.java
@@ -1,13 +1,13 @@
-// a class that is going to be used to complete a series of calculations
-// that include, adding, getting the total and subtracting as well as converting to a string
-
 package cse360assignment02;
+import java.lang.*;
 
 public class AddingMachine {
   private int total;
+  StringBuffer history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = new StringBuffer("0");
   }
   
   // should return the current total
@@ -18,21 +18,23 @@ public class AddingMachine {
   // should add the parameter to the total variable
   public void add (int value) {
     value += total;
+    history.append(" + " + value);
   }
 
   // should subtract the parameter from the total variable
   public void subtract (int value) {
     value -= total;
+    history.append(" - " + value)
   }
 
   public String toString () {
-    return " ";
+    return history.toString();
   }
 
   // should clear the "memory"
   public void clear() {
     total = 0;
-
+    history.delete(0, history.length());
   }
 
 }
--- Assignment02/AddingMachinev1.java
+++ Assignment02/AddingMachinev1.java
@@ -1,13 +1,13 @@
-package cse360assignment02;
-import java.lang.*;
+// a class that is going to be used to complete a series of calculations
+// that include, adding, getting the total and subtracting as well as converting to a string
 
-public class AddingMachinev2 {
+package cse360assignment02;
+
+public class AddingMachinev1 {
   private int total;
-  StringBuffer history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
-    history = new StringBuffer("0");
   }
   
   // should return the current total
@@ -18,23 +18,21 @@ public class AddingMachinev2 {
   // should add the parameter to the total variable
   public void add (int value) {
     value += total;
-    history.append(" + " + value);
   }
 
   // should subtract the parameter from the total variable
   public void subtract (int value) {
     value -= total;
-    history.append(" - " + value)
   }
 
   public String toString () {
-    return history.toString();
+    return " ";
   }
 
   // should clear the "memory"
   public void clear() {
     total = 0;
-    history.delete(0, history.length());
+
   }
 
 }
